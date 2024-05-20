f040df6ac8a63723b50d8fe8ea6523ff82e1f2cb
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-05-20 15:18:26 -0300

    update

--- nobuild.c
+++ nobuild.c
@@ -9,8 +9,17 @@ int main(int argc, char **argv) {
   if (!path_exists("build")) {
     MKDIRS("build");
   }
-  CMD("cc", CFLAGS, "-o", "build/crafti", "src/main.c");
-  CMD("./build/crafti");
+  //if (!path_exists("bin")) {
+  //  MKDIRS("bin");
+  //}
+  //FOREACH_FILE_IN_DIR(file, "src", {
+  //  if (ENDS_WITH(file, ".c"))
+  //  CMD("cc", CFLAGS, "-o", CONCAT("bin/",NOEXT(file), ".o"), CONCAT("src/", file), "-c", "-Iinclude");
+  //});
+
+  //CMD("cc", CFLAGS, "-o", "build/crafti", "bin/main.o", "bin/chunk.o", "bin/debug.o", "bin/vm.o");
+  CMD("cc", CFLAGS, "-o", "build/crafti", "src/main.c", "src/debug.c", "src/vm.c", "src/compiler.c", "-Iinclude");
+  //CMD("./build/crafti");
 
   return 0;
 }
